963518a6d1cff0d1a6dd02a450b65a2884b51ff2
Action <theactionbreaker@gmail.com>
2016-05-14 14:52:04 +0200

    v1.0
    Initial release

---
+++ Fonctions.cs
@@ -0,0 +1,103 @@
+﻿using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace SDVSaveUpdater2
+{
+    class Fonctions
+    {
+        Data mData;
+
+        public Fonctions()
+        {
+            mData = new Data();
+        }
+
+        public bool RemplaceDraivin(XDocument Name, XDocument Game)
+        {
+
+            var leaves =
+                from e in Name.Descendants()
+                where !e.Elements().Any()
+                select e;
+
+            for (int i = 0; i < mData.tablefr.GetLength(0); i++)
+            {
+                foreach (var leaf in leaves)
+                {
+                    var value = leaf.Value;
+                    if (value == mData.tablefr[i, 0])
+                    {
+                        value = mData.tablefr[i, 1];
+                    }
+
+                    leaf.Value = value;
+                }
+            }
+            var leavess =
+                from e in Game.Descendants()
+                where !e.Elements().Any()
+                select e;
+
+            for (int i = 0; i < mData.tablefr.GetLength(0); i++)
+            {
+                foreach (var leaff in leavess)
+                {
+                    var value = leaff.Value;
+                    if (value == mData.tablefr[i, 0])
+                    {
+                        value = mData.tablefr[i, 1];
+                    }
+
+                    leaff.Value = value;
+                }
+            }
+
+            return true;
+        }
+
+        public string Analyse(XDocument Name, XDocument Game)
+        {
+            string result = "Votre save est en anglais";
+
+            var leaves =
+                from e in Name.Descendants()
+                where !e.Elements().Any()
+                select e;
+            foreach (var leaf in leaves)    // Parcours save
+            {
+                var value = leaf.Value;     // Value = nom dans save
+                if (value == "Grande Grange")
+                {
+                    result = "Votre save a besoin d'être corrigé";
+                }
+                if ((value == "Faux") && (result != "Votre save a besoin d'être corrigé"))
+                {
+                    result = "Votre save est compatible";
+                }
+            }
+
+            var leavess =
+                from e in Game.Descendants()
+                where !e.Elements().Any()
+                select e;
+            foreach (var leaff in leavess)    // Parcours save
+            {
+                var value = leaff.Value;     // Value = nom dans save
+                if (value == "Grande Grange")
+                {
+                    result = "Votre save a besoin d'être corrigé";
+                }
+                if ((value == "Faux") && (result != "Votre save a besoin d'être corrigé"))
+                {
+                    result = "Votre save est compatible";
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace SDVSaveUpdater2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonReplace = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.labelAnalyse = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonReplace
+            // 
+            this.buttonReplace.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.buttonReplace.FlatAppearance.BorderSize = 0;
+            this.buttonReplace.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonReplace.Font = new System.Drawing.Font("Calibri", 15F);
+            this.buttonReplace.ForeColor = System.Drawing.Color.White;
+            this.buttonReplace.Location = new System.Drawing.Point(52, 145);
+            this.buttonReplace.Name = "buttonReplace";
+            this.buttonReplace.Size = new System.Drawing.Size(256, 53);
+            this.buttonReplace.TabIndex = 2;
+            this.buttonReplace.Text = "Corriger";
+            this.buttonReplace.UseVisualStyleBackColor = false;
+            this.buttonReplace.Click += new System.EventHandler(this.buttonReplace_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 11F);
+            this.label1.Location = new System.Drawing.Point(59, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 23);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Rendez vos saves compatibles";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(120, 51);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 19;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // labelAnalyse
+            // 
+            this.labelAnalyse.Font = new System.Drawing.Font("Calibri", 11F);
+            this.labelAnalyse.Location = new System.Drawing.Point(2, 92);
+            this.labelAnalyse.Name = "labelAnalyse";
+            this.labelAnalyse.Size = new System.Drawing.Size(356, 28);
+            this.labelAnalyse.TabIndex = 20;
+            this.labelAnalyse.Text = "...";
+            this.labelAnalyse.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 11F);
+            this.label2.Location = new System.Drawing.Point(105, 232);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 23);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Par la team SDVFR";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(360, 265);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelAnalyse);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonReplace);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SaveUpdater";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonReplace;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label labelAnalyse;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
