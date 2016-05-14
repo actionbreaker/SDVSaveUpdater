8f4efa68f850557538fb959e30c153ff69c9ad00
Action <theactionbreaker@gmail.com>
2016-05-14 14:57:05 +0200

    Revert "Revert "v1.0""
    This reverts commit 56374fd644b51410dc68dc7e0e761148e0490723.

---
+++ Data.cs
@@ -0,0 +1,16 @@
+﻿namespace SDVSaveUpdater2
+{
+    class Data
+    {
+        public string[,] tablefr = new string[,]
+        {
+            // Fix
+            {"Grande Grange",  "Grand Grange"},
+        };
+
+        public Data()
+        {
+
+        }
+    }
+}
